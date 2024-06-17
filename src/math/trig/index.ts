dc94671357ef1b42fda2c60e2ca06f404b2dc93d
Hans Oksendahl <hansoksendahl@gmail.com>
2024-06-16 19:10:13 -0700

    added a function that was left out

--- src/math/trig/index.ts
+++ src/math/trig/index.ts
@@ -1,3 +1,4 @@
 export { default as addAngles } from './add-angles'
 export { default as degToRad } from './deg-to-rad'
+export { default as getAngle } from './get-angle'
 export { default as radToDeg } from './rad-to-deg'
